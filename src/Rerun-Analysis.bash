21904825816b76f4bb2772bac11f7685bf298a0a
Yang Liao <Yang.Liao@onjcri.org.au>
2020-07-01 20:20:38 +1000

    Added rerun bash

---
+++ src/Rerun-Analysis.bash
@@ -0,0 +1,7 @@
+bash Pre1-Trimming-Reads.bash
+bash Pre2-Mapping-Reads-Subread.bash
+bash Pre3-Mapping-Reads-STAR.bash
+bash Tab1-Mapped-Reads.bash
+bash Tab2-Coef-with-Truth.bash
+bash SupplTab1-Trimmed-Bases.bash
+bash SupplTab2-Adapter-Clipped.bash
